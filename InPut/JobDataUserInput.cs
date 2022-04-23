d411fc3fa939956df77e423106ecf0df6fa2083b
OrMeirchak <89354961+OrMeirchak@users.noreply.github.com>
2022-04-23 16:11:18 +0300

    Add files via upload

---
+++ InPut/JobDataUserInput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalenyaTest.InPut
+{
+    class JobDataUserInput:Input{
+
+        public string txtFile { get; }
+        public string txtFileName { get; }
+        public string jsonFile { get; }
+        public string jsonFileName { get; }
+
+        //The constructor asks the user to type input and uses a UserInputReader to get the files from the user
+        public JobDataUserInput()
+        {
+            Console.WriteLine("Please type the full path of the text file");
+            UserInputReader txtFileReader = new UserInputReader(".txt");
+            txtFileName = txtFileReader.fileName;
+            txtFile = txtFileReader.file;
+
+            Console.WriteLine("Please type the full path of the json file");
+            UserInputReader jsonFileReader = new UserInputReader(".json");
+            jsonFileName = jsonFileReader.fileName;
+            jsonFile = jsonFileReader.file;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalenyaTest.InPut;
+using TalenyaTest.Processing;
+
+namespace TalenyaTest
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Input input;
+            Processor proccesor;
+
+            //In case you want to type the path as a variable
+            //input = new JobDataLocalVarInput(@"",@"");
+
+            //In case you want to type the path to the console
+            input = new JobDataUserInput();
+
+            proccesor = new JobDataProcessor(input);
+
+            proccesor.writeOutputToFile();
+        }
+    } }
+
+  
+
+
